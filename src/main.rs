51b21dd0b1c2deee9bd110571fd2d0a71cb3255c
Dietmar Maurer <dietmar@proxmox.com>
2023-03-12 12:00:40 +0100

    add spam list

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,22 @@
 mod top_nav_bar;
 pub use top_nav_bar::TopNavBar;
 
+mod spam_list;
+pub use spam_list::SpamList;
+
 use log::Log;
+use percent_encoding::percent_decode_str;
+
 use yew::prelude::*;
 use yew_router::HashRouter;
 
 use pwt::prelude::*;
 use pwt::widget::{Column, Dialog, ThemeLoader};
 
-use proxmox_yew_comp::HttpClient;
+use proxmox_yew_comp::http_login;
 use proxmox_yew_comp::LoginInfo;
 use proxmox_yew_comp::LoginPanel;
-use proxmox_yew_comp::http_login;
+use proxmox_yew_comp::ProxmoxProduct;
 
 enum Msg {
     Login(LoginInfo),
@@ -26,12 +31,12 @@ impl PmgQuarantineApp {
     fn ticket_login(ctx: &Context<Self>) {
         let link = ctx.link().clone();
         wasm_bindgen_futures::spawn_local(async move {
-            //https://proxmox-new.maurer-it.com:8006/quarantine?ticket=PMGQUAR%253Adietmar%2540proxmox.com%253A640BB7AD%253A%253ALOPdDlq1oUdi3XxeXuvxU5wPBf4P15CMWjuKIYAX03AXaZIXa%252BGsbvolOn%252BrJ8zTDm7YY1aXfpcTnCIeLuCDLMbDa5qUGuQ2W30Q1xYUuTqY%252Bi8npc6%252BRLib%252FVCbk1a7hB6S5b4k0L7WQZEWefMJgXc6DcVovKRVBBZQ90Fs9gHXxuCK9yS1D4qKoBmMsJipwjnYbuvjZFmRU4m%252BNfu6hwIju%252BOzJNhFq0g1eXV63H8lWjLyCwlb%252BPq%252BZKtvZ5XxPJS9T8Pv%252FAgzCPsdre56oK1FJ%252Byd7EOZDpLkMpLg3pHSr97Ezze5m3R3sfI7Ye4ZZ0NhvbDZhCcGc3XX9wbtlg%253D%253D&cselect=C1R67412T90485159&date=2023-03-10
-            let username = "dietmar";
-            let realm = "proxmox.com";
-            let ticket = "PMGQUAR:dietmar@proxmox.com:640BB7AD::LOPdDlq1oUdi3XxeXuvxU5wPBf4P15CMWjuKIYAX03AXaZIXa+GsbvolOn+rJ8zTDm7YY1aXfpcTnCIeLuCDLMbDa5qUGuQ2W30Q1xYUuTqY+i8npc6+RLib/VCbk1a7hB6S5b4k0L7WQZEWefMJgXc6DcVovKRVBBZQ90Fs9gHXxuCK9yS1D4qKoBmMsJipwjnYbuvjZFmRU4m+Nfu6hwIju+OzJNhFq0g1eXV63H8lWjLyCwlb+Pq+ZKtvZ5XxPJS9T8Pv/AgzCPsdre56oK1FJ+yd7EOZDpLkMpLg3pHSr97Ezze5m3R3sfI7Ye4ZZ0NhvbDZhCcGc3XX9wbtlg==";
+            let username = "dietmar@proxmox.com";
+            let ticket  = percent_decode_str("PMGQUAR%253Adietmar%2540proxmox.com%253A640D0922%253A%253ARHD4wNz7F%252BVWFeOTlCWGJLi43oR5mJWw3HrDr7EvBb9azzRMAdSrcJztKLj%252BUfPjnG7R%252BQkPJyrlDkyE3pW8fInjjVrn0bNuDiVN7Y9GN1OUsXsC0noDCUcVhTU1TRQv2XLp%252Bur8Sz8gGTspKib%252F4StwZxltKa78RZyOqv716Lo5x6o4MbuYHcXZfIYcRtMZlgJEUhE710UNHQ0YU5dv1F7uGKw0koI7QaWYv00Di7cmsm1VJCsO5XYyFLVe8dOv8uuHUMf8Mt4Etw7BOQjzOWYJhXc%252Bkf4wb8nHFME%252BisFbUhf4qN9RN9nGAmTTut%252F6i1tdUwGnHtM4tbTVegJ6KA%253D%253D")
+                     .decode_utf8_lossy();
+            let ticket = percent_decode_str(&ticket).decode_utf8_lossy();
 
-            match http_login(username, ticket, realm).await {
+            match http_login(username, ticket, "quarantine").await {
                 Ok(info) => {
                     link.send_message(Msg::Login(info));
                 }
@@ -49,7 +54,7 @@ impl Component for PmgQuarantineApp {
     type Properties = ();
 
     fn create(ctx: &Context<Self>) -> Self {
-        let login_info = LoginInfo::from_cookie();
+        let login_info = LoginInfo::from_cookie(ProxmoxProduct::PMG);
         if let Some(info) = &login_info {
             log::info!("GOT COOKIE");
             proxmox_yew_comp::http_set_auth(info.clone());
@@ -79,8 +84,10 @@ impl Component for PmgQuarantineApp {
     fn view(&self, ctx: &Context<Self>) -> Html {
         let onlogin = ctx.link().callback(|info| Msg::Login(info));
 
-        let body = Column::new().class("pwt-viewport")
+        let body = Column::new()
+            .class("pwt-viewport")
             .with_child(TopNavBar::new())
+            .with_child(SpamList::new())
             .with_child("TEST PMG");
 
         ThemeLoader::new(body).into()
@@ -93,6 +100,8 @@ fn Scafold() -> Html {
 }
 
 fn main() {
+    proxmox_yew_comp::http_setup(proxmox_yew_comp::ProxmoxProduct::PMG);
+
     pwt::props::set_http_get_method(
         |url| async move { proxmox_yew_comp::http_get(&url, None).await },
     );
---
+++ src/spam_list.rs
@@ -0,0 +1,106 @@
+use serde_json::Value;
+use std::rc::Rc;
+
+use pwt::prelude::*;
+use yew::virtual_dom::{VComp, VNode};
+//use yew::html::IntoEventCallback;
+
+use proxmox_yew_comp::http_get;
+use pwt::widget::Column;
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct MailInfo {
+    pub bytes: i64,
+    pub from: String,
+    pub id: String,
+    pub subject: String,
+    pub receiver: String,
+    pub envelope_sender: String,
+    pub spamlevel: i64,
+    pub time: u64,
+}
+#[derive(Clone, PartialEq, Properties)]
+pub struct SpamList {}
+
+impl SpamList {
+    pub fn new() -> Self {
+        yew::props!(Self {})
+    }
+}
+
+pub enum Msg {
+    LoadData(Vec<MailInfo>),
+}
+
+pub struct PmgSpamList {
+    data: Vec<MailInfo>,
+}
+
+impl PmgSpamList {
+    fn load(&self, ctx: &Context<Self>) {
+        let link = ctx.link().clone();
+        wasm_bindgen_futures::spawn_local(async move {
+            match http_get::<Vec<MailInfo>>("/quarantine/spam", None).await {
+                Ok(data) => {
+                    link.send_message(Msg::LoadData(data));
+                }
+                Err(err) => {
+                    log::error!("ERROR: {:?}", err);
+                    //link.send_message(Msg::LoginError(err.to_string()));
+                }
+            }
+        })
+    }
+}
+
+impl Component for PmgSpamList {
+    type Message = Msg;
+    type Properties = SpamList;
+
+    fn create(ctx: &Context<Self>) -> Self {
+        let me = Self { data: Vec::new() };
+        me.load(ctx);
+        me
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::LoadData(data) => {
+                log::info!("GOT {:?}", data);
+                self.data = data;
+            }
+        }
+        true
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let children: Vec<Html> = self.data.iter()
+            .map(|item| {
+                Column::new()
+                    .padding_x(2)
+                    .padding_y(1)
+                    .border_bottom(true)
+                    .with_child(html!{
+                        <div class="pwt-font-label-small pwt-text-truncate">{&item.from}</div>
+                    })
+                    .with_child(html!{
+                        <div class="pwt-font-title-medium pwt-text-truncate">{&item.subject}</div>
+                    })
+                    .into()
+            })
+            .collect();
+
+        Column::new()
+            .class("pwt-fit")
+            .children(children)
+            .into()
+    }
+}
+
+impl Into<VNode> for SpamList {
+    fn into(self) -> VNode {
+        let comp = VComp::new::<PmgSpamList>(Rc::new(self), None);
+        VNode::from(comp)
+    }
+}
