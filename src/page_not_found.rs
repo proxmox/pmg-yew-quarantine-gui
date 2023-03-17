134a024fdd00498286d03bab6e620b246e9ac5a8
Dietmar Maurer <dietmar@proxmox.com>
2023-03-17 12:49:10 +0100

    cleanup

--- src/page_not_found.rs
+++ src/page_not_found.rs
@@ -5,13 +5,11 @@ use crate::TopNavBar;
 
 #[function_component]
 pub fn PageNotFound() -> Html {
-
     let content = error_message("page not found", "pwt-p-2");
 
     Column::new()
-    .class("pwt-viewport")
-    .with_child(TopNavBar::new())
-    .with_child(content)
-
-    .into()
+        .class("pwt-viewport")
+        .with_child(TopNavBar::new())
+        .with_child(content)
+        .into()
 }
