c466c38cbb227aa461e1f0d61730066018e97ad7
Dominik Csapak <d.csapak@proxmox.com>
2025-07-02 15:39:32 +0200

    spam list: redesign list items
    this makes it much more like the list in the current PMG quarantine
    mobile ui made with framework7:
    
    * show a progress bar during intial load
    * make the items slidable
    * adapt the font size
    * show chevron-right to indicate the tiles are clickable
    * use List to take advantage of virtual scrolling
    * show date grouping header
    
    Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

--- src/spam_list.rs
+++ src/spam_list.rs
@@ -1,9 +1,17 @@
-use anyhow::{format_err, Error};
+use anyhow::Error;
+use wasm_bindgen::JsValue;
 
+use core::clone::Clone;
+use js_sys::Date;
 use serde_json::Value;
 use std::rc::Rc;
 
-use pwt::{prelude::*, widget::error_message};
+use pwt::{
+    css::{AlignItems, ColorScheme, FlexFit, Opacity, Overflow},
+    prelude::*,
+    touch::{Slidable, SlidableAction, SnackBar, SnackBarContextExt},
+    widget::{error_message, Container, Fa, List, ListTile, Progress, Row},
+};
 use yew::{
     html::{IntoEventCallback, IntoPropValue},
     virtual_dom::{VComp, VNode},
@@ -11,9 +19,11 @@ use yew::{
 //use yew::html::IntoEventCallback;
 
 use proxmox_yew_comp::http_get;
-use pwt::widget::{Card, Column};
+use pwt::widget::Column;
 
 use serde::{Deserialize, Serialize};
+
+use crate::{mail_action, MailAction};
 
 #[derive(Copy, Clone, Serialize, Default, PartialEq)]
 pub struct SpamListParam {
@@ -23,17 +33,24 @@ pub struct SpamListParam {
     endtime: Option<u64>,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub struct MailInfo {
-    pub bytes: i64,
+    //pub bytes: i64,
     pub from: String,
     pub id: String,
     pub subject: String,
-    pub receiver: String,
-    pub envelope_sender: String,
+    //pub receiver: String,
+    //pub envelope_sender: String,
     pub spamlevel: i64,
-    pub time: u64,
+    pub time: i64,
 }
+
+#[derive(Clone)]
+pub enum ListEntry {
+    Date(String),
+    Mail(MailInfo),
+}
+
 #[derive(Clone, PartialEq, Properties)]
 pub struct SpamList {
     #[prop_or_default]
@@ -64,11 +81,13 @@ impl SpamList {
 }
 
 pub enum Msg {
+    Reload,
+    Action(String, MailAction), // id
     LoadResult(Result<Vec<MailInfo>, Error>),
 }
 
 pub struct PmgSpamList {
-    data: Result<Vec<MailInfo>, Error>,
+    data: Option<Result<Vec<ListEntry>, Error>>,
 }
 
 impl PmgSpamList {
@@ -79,45 +98,9 @@ impl PmgSpamList {
 
         wasm_bindgen_futures::spawn_local(async move {
             let result = http_get::<Vec<MailInfo>>("/quarantine/spam", Some(param)).await;
+
             link.send_message(Msg::LoadResult(result));
         })
-    }
-
-    fn render_list_item(&self, ctx: &Context<Self>, item: &MailInfo) -> Html {
-        Card::new()
-            .class("pwt-d-flex")
-            .class("pwt-shape-none pwt-card-flat pwt-interactive")
-            .class("pwt-scheme-neutral")
-            .padding_x(2)
-            .padding_y(1)
-            .border_bottom(true)
-            .class("pwt-align-items-center")
-            .with_child(
-                Column::new()
-                    .class("pwt-fit")
-                    .class("pwt-pe-1")
-                    .with_child(html! {
-                        <div class="pwt-font-label-small pwt-text-truncate">{&item.from}</div>
-                    })
-                    .with_child(html! {
-                        <div class="pwt-font-title-medium pwt-text-truncate">{&item.subject}</div>
-                    }),
-            )
-            .with_child(html! {
-                <div class="pwt-white-space-nowrap">
-                {format!("Score: {}", item.spamlevel)}
-                </div>
-            })
-            .onclick(Callback::from({
-                let id = item.id.clone();
-                let on_preview = ctx.props().on_preview.clone();
-                move |_| {
-                    if let Some(on_preview) = &on_preview {
-                        on_preview.emit(id.clone());
-                    }
-                }
-            }))
-            .into()
     }
 }
 
@@ -126,17 +109,48 @@ impl Component for PmgSpamList {
     type Properties = SpamList;
 
     fn create(ctx: &Context<Self>) -> Self {
-        let me = Self {
-            data: Err(format_err!("no data loaded")),
-        };
+        let me = Self { data: None };
         me.load(ctx);
         me
     }
 
-    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
-            Msg::LoadResult(result) => {
-                self.data = result;
+            Msg::Reload => {
+                self.load(ctx);
+            }
+            Msg::LoadResult(result) => match result {
+                Ok(mut data) => {
+                    data.sort_by(|a, b| match b.time.cmp(&a.time) {
+                        core::cmp::Ordering::Equal => a.cmp(b),
+                        other => other,
+                    });
+
+                    let mut res = Vec::new();
+                    let mut last_date = String::new();
+
+                    for mail in data {
+                        let date = epoch_to_date(mail.time);
+                        if date != last_date {
+                            res.push(ListEntry::Date(date.clone()));
+                            last_date = date;
+                        }
+                        res.push(ListEntry::Mail(mail));
+                    }
+
+                    self.data = Some(Ok(res));
+                }
+                Err(err) => self.data = Some(Err(err)),
+            },
+            Msg::Action(id, action) => {
+                let link = ctx.link().clone();
+                wasm_bindgen_futures::spawn_local(async move {
+                    if let Err(err) = mail_action(&id, action).await {
+                        link.show_snackbar(SnackBar::new().message(err.to_string()));
+                    }
+                    link.send_message(Msg::Reload);
+                });
+                return false;
             }
         }
         true
@@ -154,15 +168,22 @@ impl Component for PmgSpamList {
 
     fn view(&self, ctx: &Context<Self>) -> Html {
         match &self.data {
-            Ok(data) => {
-                let children: Vec<Html> = data
-                    .iter()
-                    .map(|item| self.render_list_item(ctx, item))
-                    .collect();
-
-                Column::new().class("pwt-fit").children(children).into()
+            Some(Ok(data)) => {
+                let on_preview = ctx.props().on_preview.clone();
+                let data = data.clone();
+                let link = ctx.link().clone();
+                List::new(data.len() as u64, move |pos: u64| {
+                    ListTile::new().padding(0).with_child(render_list_item(
+                        &link,
+                        on_preview.clone(),
+                        &data[pos as usize],
+                    ))
+                })
+                .class(FlexFit)
+                .into()
             }
-            Err(err) => error_message(&err.to_string()).into(),
+            Some(Err(err)) => error_message(&err.to_string()).into(),
+            None => Progress::new().into(),
         }
     }
 }
@@ -172,4 +193,105 @@ impl From<SpamList> for VNode {
         let comp = VComp::new::<PmgSpamList>(Rc::new(val), None);
         VNode::from(comp)
     }
+}
+
+fn render_list_item(
+    link: &yew::html::Scope<PmgSpamList>,
+    on_preview: Option<Callback<String>>,
+    item: &ListEntry,
+) -> Html {
+    match item {
+        ListEntry::Date(date) => Container::new()
+            .padding(1)
+            .class(ColorScheme::Surface)
+            .class("pwt-default-colors")
+            .with_child(date)
+            .into(),
+        ListEntry::Mail(item) => {
+            let make_cb = |action: MailAction| {
+                let id = item.id.clone();
+                link.callback(move |_| Msg::Action(id.clone(), action))
+            };
+
+            let content = Column::new()
+                .class(FlexFit)
+                .with_child(html! {
+                    <div class="pwt-font-label-small pwt-text-truncate">{&item.from}</div>
+                })
+                .with_child(html! {
+                    <div class="pwt-font-title-small pwt-text-truncate">{&item.subject}</div>
+                });
+            let score = Container::new()
+                .class("pwt-white-space-nowrap")
+                .class(Opacity::Half)
+                .with_child(tr!("Score: {0}", item.spamlevel));
+            let main = Row::new()
+                .class(FlexFit)
+                .gap(1)
+                .padding_x(2)
+                .padding_y(1)
+                .border_bottom(true)
+                .class(AlignItems::Center)
+                .style("cursor", "pointer")
+                .with_child(content)
+                .with_child(score)
+                .with_child(Fa::new("chevron-right").class(Opacity::Half));
+
+            Slidable::new(main)
+                .class(Overflow::Auto)
+                .on_tap({
+                    let id = item.id.clone();
+                    let on_preview = on_preview.clone();
+                    move |_| {
+                        if let Some(on_preview) = &on_preview {
+                            on_preview.emit(id.clone())
+                        }
+                    }
+                })
+                .left_actions(
+                    Row::new()
+                        .style("height", "100%") // FIXME better solved in scss of slidable?
+                        .class(AlignItems::Center)
+                        .with_child(
+                            SlidableAction::new(tr!("Deliver"))
+                                .class(ColorScheme::SuccessContainer)
+                                .icon_class("fa fa-paper-plane")
+                                .on_activate(make_cb(MailAction::Deliver)),
+                        )
+                        .with_child(
+                            SlidableAction::new(tr!("Whitelist"))
+                                .icon_class("fa fa-check")
+                                .on_activate(make_cb(MailAction::Whitelist)),
+                        ),
+                )
+                .right_actions(
+                    Row::new()
+                        .style("height", "100%") // FIXME better solved in scss of slidable?
+                        .class(AlignItems::Center)
+                        .with_child(
+                            SlidableAction::new(tr!("Blacklist"))
+                                .class(ColorScheme::WarningContainer)
+                                .icon_class("fa fa-times")
+                                .on_activate(make_cb(MailAction::Blacklist)),
+                        )
+                        .with_child(
+                            SlidableAction::new(tr!("Delete"))
+                                .class(ColorScheme::ErrorContainer)
+                                .icon_class("fa fa-trash")
+                                .on_activate(make_cb(MailAction::Delete)),
+                        ),
+                )
+                .into()
+        }
+    }
+}
+
+fn epoch_to_date(epoch: i64) -> String {
+    let date = Date::new(&JsValue::from_f64(1000.0 * epoch as f64));
+    format!(
+        "{:04}-{:02}-{:02}",
+        date.get_full_year(),
+        date.get_month() + 1,
+        date.get_date()
+    )
 }
